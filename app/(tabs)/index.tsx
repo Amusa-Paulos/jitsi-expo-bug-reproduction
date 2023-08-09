b9e4549dcbb3d074042e9c5725dda9edfd124886
Abayomi Amusa <amusaabayomipaul99@gmail.com>
2023-08-09 22:20:00 +0100

    just a commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,13 +2,36 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import {JitsiMeeting} from '@jitsi/react-native-sdk/index';
+import React from 'react';
 
 export default function TabOneScreen() {
+  const jitsiMeeting = React.useRef(null);
+
+
+  const onReadyToClose = React.useCallback(() => {
+    // @ts-ignore
+    jitsiMeeting.current.close();
+  }, []);
+
+  const eventListeners = {
+    onReadyToClose
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      {/* <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" /> */}
+      {/* <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
+      {
+        // @ts-ignore
+        <JitsiMeeting
+          eventListeners={eventListeners as any}
+          ref={jitsiMeeting}
+          style={{flex: 1, height: 700, width: '100%'}}
+          room={'blahblahblah'}
+          serverURL={'https://meet.jit.si/'} />
+      }
     </View>
   );
 }
